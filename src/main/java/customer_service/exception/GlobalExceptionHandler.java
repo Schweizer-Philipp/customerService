a456016abbe215f9d092b3779540749120348829
Philipp Schweizer <Philipp.Schw@directbox.com>
2024-01-20 16:09:27 +0100

    feat: validation for add customer request

--- src/main/java/customer_service/exception/GlobalExceptionHandler.java
+++ src/main/java/customer_service/exception/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package customer_service.exception;
 
 import customer_service.model.CustomerServiceException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -15,5 +21,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomerAlreadyExistException.class)
     protected final ResponseEntity<CustomerServiceException> handleException(CustomerAlreadyExistException ex) {
         return new ResponseEntity<>(new CustomerServiceException(List.of(ex.getMessage())), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errorList = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(new CustomerServiceException(errorList), HttpStatus.BAD_REQUEST);
     }
 }
